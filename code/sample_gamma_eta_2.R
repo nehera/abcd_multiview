23b28053ee59f53d389f6fd7b0068d66e35fc025
Aidan Neher <atm.neher@gmail.com>
2023-09-05 17:31:22 -0500

    Update simulate_simple_data.R and initialize sample_gamma_eta_2.R
    RE simulate_simple_data.R - Removed some hardcoding in Thierry's original simulate() function.
    
    RE initialize sample_gamma_eta_2.R - Did this to simplify the changepoints (if else statements) in the algorithm's for loop.

---
+++ code/sample_gamma_eta_2.R
@@ -0,0 +1,188 @@
+library(tidyverse)
+
+r <- 4
+n <- 200
+p_m <- 500
+prob_component_selection <- 0.5
+prob_feature_selection <- 0.05
+
+initialize_gamma <- function(r=4, prior_component_selection=0.5) {
+  gamma <- rbinom(n = r, size = 1, prob = prior_component_selection)
+  return(gamma)
+}
+initialize_eta <- function(gamma, r=4, p_m=500, prior_feature_selection=0.05) {
+  eta <- matrix(nrow = r, ncol = p_m)
+  for (l in 1:r) {
+    if (gamma[l] == 1) {
+      eta[l, ] <- rbinom(n = p_m, size = 1, prior_feature_selection)
+    } else {
+      eta[l, ] <- rep(0, p_m)
+    }
+  }
+  return(eta)
+}
+
+set.seed(1)
+gamma <- initialize_gamma()
+eta <- initialize_eta(gamma)
+
+sigma2 <- rep(1, p_m)
+tau2 <- matrix(1, nrow = r, ncol = p_m)
+simulation_results <- readRDS("data/2023-07-03_simulation_results.rds")
+U <- simulation_results$U # U is a fixed set of covariates
+
+m <- 1 # Start with the 1st view
+data_list <- readRDS("data/2023-07-03_simulation_data_list.rds")
+x <- data_list[[m]]
+
+## -- calculate P_lj
+calculate_mvnorm_var_j <- function(eta_j, sigma2_j, tau2_j, U, n) {
+  n_components_active_in <- sum(eta_j)
+  if (n_components_active_in > 0) {
+    components_active_in <- which(eta_j==1)
+    Sigma2_j <- U[, components_active_in, drop = FALSE] %*% 
+      diag(tau2_j[components_active_in]) %*% 
+      t(U[, components_active_in, drop = FALSE]) + diag(n) # TODO use Woodbury matrix formula for inversion
+  } else {
+    Sigma2_j <- diag(n)
+  }
+  mvnorm_var <- sigma2_j * Sigma2_j
+  return(mvnorm_var)
+}
+
+calculate_log_dmvnorm_j <- function(eta_j, sigma2_j, tau2_j, U, n, x_j) {
+  mvnorm_var_j <- calculate_mvnorm_var_j(eta_j, sigma2_j, tau2_j, U, n)
+  log_dmvnorm_j <- mvtnorm::dmvnorm(x = x_j, mean = rep(0, n), sigma = mvnorm_var_j, log = TRUE)
+  return(log_dmvnorm_j)
+}
+
+calculate_log_G_j <- function(gamma, eta_j, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection) {
+  log_dmvnorm_j <- calculate_log_dmvnorm_j(eta_j, sigma2_j, tau2_j, U, n, x_j)
+  active_gamma <- which(gamma == 1)
+  n_active_gamma <- length(active_gamma) # TODO account for all inactive
+  n_active_eta_given_gamma_1 <- eta_j[active_gamma] %>% sum()
+  log_G_j <- log_dmvnorm_j + 
+    n_active_eta_given_gamma_1 * log(prob_feature_selection) + 
+    (n_active_gamma - n_active_eta_given_gamma_1) * log(1 - prob_feature_selection)
+  return(log_G_j) 
+}
+
+calculate_P_lj <- function(l, gamma, eta_j, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection) {
+  gamma_1 <- replace(gamma, l, 1)
+  eta_1 <- replace(eta_j, l, 1)
+  eta_0 <- replace(eta_j, l, 0)
+  log_G_j_1 <- calculate_log_G_j(gamma_1, eta_1, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection)
+  log_G_j_0 <- calculate_log_G_j(gamma_1, eta_0, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection)
+  max_arg <- max(log_G_j_1, log_G_j_0) # Use logsumexp trick
+  a <- log_G_j_1 - max_arg
+  b <- log_G_j_0 - max_arg
+  P_lj <- exp(a) / (exp(a)+exp(b))
+  return(P_lj)
+}
+
+## -- Calculate log_target_density_l 
+log_target_density_l <- function(l, gamma, eta, sigma2, tau2, U, n, p_m, 
+                                 x, prob_component_selection, prob_feature_selection) {
+  sum_log_target_density_lj <- 0
+  for (j in 1:p_m) {
+    log_dmvnorm_j <- calculate_log_dmvnorm_j(eta[, j], sigma2[j], tau2[,j], U, n, x[,j])
+    # TODO understand if the prior probablity or P_lj should be used in place of prob_feature_selection
+    sum_log_target_density_lj <- sum_log_target_density_lj + log_dmvnorm_j + 
+      eta[l,j]*log(prob_feature_selection) + (1-eta[l,j])*log(1-prob_feature_selection)
+  }
+  log_target_density_l <- gamma[l]*log(prob_component_selection) + 
+    (1-gamma[l])*log(1-prob_component_selection) + sum_log_target_density_lj
+  return(log_target_density_l)
+}
+
+
+## Testing
+# log_target_0 <- log_target_density_l(l, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
+# 
+# gamma_1 <- replace(gamma, l, 1)
+# eta_1 <- eta
+# eta_1[l, ] <- rbinom(n = p_m, size = 1, prob = prob_feature_selection)
+# 
+# log_target_1 <- log_target_density_l(l, gamma_1, eta_1, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
+# 
+# log_target_0 - log_target_1 # Note, the absolute difference is MUCH smaller in this implementation :-) 
+
+## -- Calculate log_proposal_l
+log_proposal_l_density <- function(l, gamma_prime, eta_prime, gamma, eta,
+                                   sigma2, tau2, U, n, p_m, x, prob_feature_selection) {
+  if (gamma[l]==1 & gamma_prime[l]==0 & sum(eta_prime[l,])==0) {
+    return(0)
+  } else if (gamma[l]==0 & gamma_prime[l]==1) {
+    log_proposal_l_density <- 0
+    for (j in 1:p_m) {
+      P_lj <- calculate_P_lj(l, gamma_prime, eta_prime[, j], sigma2[j], tau2[, j], U, n, x[, j], prob_feature_selection)
+      log_proposal_l_density <- log_proposal_l_density + eta_prime[l,j]*log(P_lj) + (1-eta_prime[l,j])*log(1-P_lj)
+    }
+    return(log_proposal_l_density)
+  } else {
+    stop("Error in log_proposal_l_density evaluation.")
+  }
+}
+
+## Testing
+# log_proposal_forward <- log_proposal_l_density(l, gamma_1, eta_1, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
+# log_proposal_backward <- log_proposal_l_density(l, gamma, eta, gamma_1, eta_1, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
+# log_proposal_backward - log_proposal_forward
+# 
+# log_A <- log_target_1 + log_proposal_backward - log_target_0 - log_proposal_forward
+# log_A
+
+## -- MCMC 2
+n_sample <- 100
+n_burnin <- 0
+n_iterations <- n_sample + n_burnin
+
+set.seed(1)
+gamma_chain <- matrix(nrow = r, ncol = n_iterations)
+eta_chain <- array(dim = c(r, p_m, n_iterations))
+gamma <- initialize_gamma()
+eta <- initialize_eta(gamma)
+# iter <- 1
+# l <- 1
+for (iter in 1:n_iterations) {
+  for (l in 1:r) {
+    # Propose values
+    gamma_new <- replace(gamma, l, 1-gamma[l])
+    eta_new <- eta
+    if (gamma_new[l] == 0) {
+      eta_new[l, ] <- rep(0, p_m)
+    } else {
+      eta_new[l, ] <- rbinom(n = p_m, size = 1, prob = prob_feature_selection) # Understand if P_lj's should be used
+    }
+    # Calculate log acceptance ratio
+    log_target <- log_target_density_l(l, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
+    log_target_new <- log_target_density_l(l, gamma_new, eta_new, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
+    log_proposal_forward <- log_proposal_l_density(l, gamma_new, eta_new, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
+    log_proposal_backward <- log_proposal_l_density(l, gamma, eta, gamma_new, eta_new, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
+    log_acceptance_ratio <- log_target_new + log_proposal_backward - log_target - log_proposal_forward
+    if (log(runif(1)) < log_acceptance_ratio) {
+      # Accept proposed gamma and eta
+      gamma_chain[,iter] <- gamma <- gamma_new
+      eta_chain[,,iter] <- eta <- eta_new
+    } else {
+      # Reject proposed gamma and eta
+      gamma_chain[,iter] <- gamma
+      eta_chain[,,iter] <- eta
+    }
+    if (gamma[l]==1) {
+      # Gibbs sample to mix feature activation indicators
+      for (j in 1:p_m) {
+        P_lj <- calculate_P_lj(l, gamma, eta[, j], sigma2[j], tau2[, j], U, n, x[, j], prob_feature_selection)
+        if (runif(1) < P_lj) {
+          # Turn on feature
+          eta[l,j] <- 1
+        } else {
+          # Turn off feature
+          eta[l,j] <- 0
+        }
+      }
+      # Store Gibbs feature activation mixing result
+      eta_chain[,,iter] <- eta
+    }
+  }
+}
--- code/simulate_simple_data.R
+++ code/simulate_simple_data.R
@@ -1,34 +1,36 @@
-Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
-  # scenario: We have three scenarios as decrisbed in the paper. Scenario 1 has 5 settings, and scenarios 2 and 3 have both two choices: overlap and no overlap
-  # setting: Setiings for secnario 1 only. It's a total of 4 settings
+# Aidan's version:
+simulate_data <- function(scenario=1, setting=NULL, overlap=NULL, seed=1) {
+  
+  # TODO breakout scenario 1 from scenarios 2 and 3 to simplify arguments required.
+
+  # scenario: We have three scenarios as described in the paper. Scenario 1 has 5 settings, and scenarios 2 and 3 have both two choices: overlap and no overlap
+  # setting: Settings for scenario 1 only. It's a total of 4 settings
   # overlap: If yes, then there are some overlap between components, otherwise, there is no overlap. This input only works wen cenario is either 2 or 3
   # seed: Seed to generate random numbers 
-  #
+  
   if ((scenario==1) && is.null(setting)){
     stop("You must specify the setting for scenario 1")
   }
   
   if (((scenario==2) || (scenario==3)) && (is.null(overlap))){
-    stop("You must specify overlap for scenario 2 or 3. It's yes or no for overlap")
+    stop("You must specify overlap for scenario 2 or 3. For the overlap argument, the possible options are 'yes' or 'no'.")
   }
-  #  library(Matrix);
-  P=500;
-  Q=500;
-  r=4; #%4 latent components;
-  p=P/5;
+  
+  n_total_features <- 500 # TODO remove issues related to hardcoding of Thierry's function
+  n_important_features <- n_total_features/5
+  n_unimportant_features <- n_total_features - n_important_features
+  n_important_groups <- 10
+  n_features_in_group <- 10
+  
+  r <- 4 # number of latent components
+  
+  Q=500 # TODO What is Q? 
   q=Q/5;
-  Group1=matrix(0,P, 11);
-  Group1[1:10,1]=1;
-  Group1[11:20,2]=1;
-  Group1[21:30,3]=1;
-  Group1[31:40,4]=1;
-  Group1[41:50,5]=1;
-  Group1[51:60,6]=1;
-  Group1[61:70,7]=1;
-  Group1[71:80,8]=1;
-  Group1[81:90,9]=1;
-  Group1[91:100,10]=1;
-  Group1[101:P,11]=1;
+  Group1=matrix(0,n_total_features, n_important_groups+1)
+  m <- matrix(1, nrow = n_important_groups)
+  Group1[1:n_important_features, 1:n_important_groups] <- kronecker(diag(1, n_features_in_group), m) # TODO share kronecker operation with Thierry
+  Group1[(n_important_features+1):n_total_features, n_important_groups+1] <- 1
+  
   Group2=Group1;
   
   #In general, you can generate N random numbers in the interval (a,b)
@@ -38,10 +40,10 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
   a2=.5;
   b2=.3;
   #latent component
-  V1a=V1b=matrix(0,p/2,r);V2a=V2b=matrix(0,q/2,r);
+  V1a=V1b=matrix(0,n_important_features/2,r);V2a=V2b=matrix(0,q/2,r);
   for (j in 1:r){
-    V1a[,j]=runif(p/2,a1,b1);
-    V1b[,j]=runif(p/2,b2,a2);
+    V1a[,j]=runif(n_important_features/2,a1,b1);
+    V1b[,j]=runif(n_important_features/2,b2,a2);
     V11=rbind(V1a,V1b);
     # for second dataset;
     V2a[,j]=runif(q/2,a1,b1);
@@ -50,7 +52,7 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
   }
   
   #multiply effect size for main networks by 2;
-  for (j in c(1,11, 21, 31, 41, 51, 61, 71, 81, 91)){
+  for (j in seq(1, n_important_features, by = n_features_in_group)){
     V11[j,]=2*V11[j,];
     V22[j,]=2*V22[j,];
   }
@@ -65,9 +67,14 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       Sx[ii,ii]=1;
     }
   }
+  
+  #### YOU ARE HERE ####
+  # TODO convert Sx operation above to flexible kronecker product operation
+  # m <- matrix(1, nrow = n_important_groups)
+  # Group1[1:n_important_features, 1:n_important_groups] <- kronecker(diag(1, n_features_in_group), m) # TODO share kronecker operation with Thierry
   Sigma1=Matrix::bdiag(Sx,Sx,Sx,Sx,Sx,Sx,Sx,Sx,Sx,Sx);
   s=nrow(Sigma1);
-  Sigma1=Matrix::bdiag(Sigma1,diag(P-s));
+  Sigma1=Matrix::bdiag(Sigma1,diag(n_total_features-s));
   Sigma2=Sigma1;
   
   if (scenario==1) {
@@ -75,23 +82,23 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
     if (setting==1){
       #                                       %there are 11 groups; groups 1-10 are signal variables, group 11 are noise
       #                               %variables;
-      #                               %generate a P by 11 indicator matrix where there is 1 in group k if
+      #                               %generate a n_total_features by 11 indicator matrix where there is 1 in group k if
       #                               %variable j is in that group;
       
       
       #orthonormalize;
-      #A1=[orth(V11) ;zeros(P-p,4)];
+      #A1=[orth(V11) ;zeros(n_total_features-n_important_features,4)];
       #A2=[orth(V22) ;zeros(Q-q,4)];
       
       #not orthonomalize
-      A1=rbind(V11 ,matrix(0,P-p,4));
+      A1=rbind(V11 ,matrix(0,n_total_features-n_important_features,4));
       A2=rbind(V22 ,matrix(0,Q-q,4));
       
       
       A1[abs(A1)<=10^-8]=0;
       A2[abs(A2)<=10^-8]=0;
       
-      TrueVariables1=rep(0,P);
+      TrueVariables1=rep(0,n_total_features);
       TrueVariables1[abs(A1[,1])!=0]=1;
       
       TrueVariables2=rep(0,Q);
@@ -101,18 +108,18 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       # %variables contribute to associatin between X1 and X2, and to the outcome.
       #%The remaining groups are noise. 
       #%orthonormalize;
-      #%A1=[orth(V11(1:30,:)) ;zeros(P-30,4)];
+      #%A1=[orth(V11(1:30,:)) ;zeros(n_total_features-30,4)];
       #%A2=[orth(V22(1:30,:)) ;zeros(Q-30,4)];
       
       #%not orthonomalize
-      A1=rbind(V11[1:30,] ,matrix(0,P-30,4));
+      A1=rbind(V11[1:30,] ,matrix(0,n_total_features-30,4));
       A2=rbind(V22[1:30,] ,matrix(0,Q-30,4));
       
       
       A1[abs(A1)<=10^-8]=0;
       A2[abs(A2)<=10^-8]=0;
       
-      TrueVariables1=rep(0,P);
+      TrueVariables1=rep(0,n_total_features);
       TrueVariables1[abs(A1[,1])!=0]=1;
       
       TrueVariables2=rep(0,Q);
@@ -130,14 +137,14 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       V22[randvec,]=0;
       
       
-      A1=rbind(V11 ,matrix(0,P-nrow(V11),4));
+      A1=rbind(V11 ,matrix(0,n_total_features-nrow(V11),4));
       A2=rbind(V22 ,matrix(0,Q-nrow(V22),4));
       
       
       A1[abs(A1)<=10^-8]=0;
       A2[abs(A2)<=10^-8]=0;
       
-      TrueVariables1=rep(0,P);
+      TrueVariables1=rep(0,n_total_features);
       TrueVariables1[abs(A1[,1])!=0]=1;
       
       TrueVariables2=rep(0,Q);
@@ -150,7 +157,7 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
         randvec=c(randvec,sample((10*(j-1)+1):(10*j),5,1));       
       }
       
-      A1=rbind(V11[1:30,] ,matrix(0,P-30,4));
+      A1=rbind(V11[1:30,] ,matrix(0,n_total_features-30,4));
       A2=rbind(V22[1:30,] ,matrix(0,Q-30,4));
       
       V11=V11[1:30,] ;
@@ -160,13 +167,13 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       V22[randvec,]=0;
       
       
-      A1=rbind(V11 ,matrix(0,P-nrow(V11),4));
+      A1=rbind(V11 ,matrix(0,n_total_features-nrow(V11),4));
       A2=rbind(V22 ,matrix(0,Q-nrow(V22),4));
       
       A1[abs(A1)<=10^-8]=0;
       A2[abs(A2)<=10^-8]=0;
       
-      TrueVariables1=rep(0,P);
+      TrueVariables1=rep(0,n_total_features);
       TrueVariables1[abs(A1[,1])!=0]=1;
       
       TrueVariables2=rep(0,Q);
@@ -178,10 +185,10 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       
       
       
-      A1=rbind(V11 ,matrix(0,P-p,4));
+      A1=rbind(V11 ,matrix(0,n_total_features-n_important_features,4));
       A2=rbind(V22 ,matrix(0,Q-q,4));
       
-      whic=matrix(0,P,4);
+      whic=matrix(0,n_total_features,4);
       whic[1:25,1]=1;
       whic[26:50,2]=1;
       whic[56:75,3]=1; 
@@ -200,7 +207,7 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       A2[abs(A2)<=10^-8]=0;
       
       
-      TrueVariables1=rep(0,P);
+      TrueVariables1=rep(0,n_total_features);
       TrueVariables1[apply(A1,1,function(x) sum(abs(x)))!=0]=1;
       #TrueVariables1(sum(abs(A1),2)~=0)=1;
       
@@ -209,11 +216,11 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
     } 
   } else if (scenario==2){
     
-    A1=rbind(V11 ,matrix(0,P-p,4));
+    A1=rbind(V11 ,matrix(0,n_total_features-n_important_features,4));
     A2=rbind(V22 ,matrix(0,Q-q,4)); 
     if (overlap=="yes"){
       # %overlap
-      whic=matrix(0,P,4);
+      whic=matrix(0,n_total_features,4);
       whic[1:100,1]=1;
       whic[1:100,2]=1;
       whic[1:100,3]=1;
@@ -229,13 +236,13 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       
       A1[abs(A1)<=10^-8]=0;
       A2[abs(A2)<=10^-8]=0;
-      TrueVariables1=rep(0,P);
+      TrueVariables1=rep(0,n_total_features);
       TrueVariables1[apply(A1,1,function(x) sum(abs(x)))!=0]=1;
       TrueVariables2=rep(0,Q);
       TrueVariables2[apply(A2,1,function(x) sum(abs(x)))!=0]=1;
     } else if (overlap=="no"){
       #%No overlap    
-      whic=matrix(0,P,4);
+      whic=matrix(0,n_total_features,4);
       whic[1:50,1]=1;
       whic[51:100,2]=1;
       whic[1:50,3]=1;
@@ -251,7 +258,7 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       
       A1[abs(A1)<=10^-8]=0;
       A2[abs(A2)<=10^-8]=0;
-      TrueVariables1=rep(0,P);
+      TrueVariables1=rep(0,n_total_features);
       TrueVariables1[apply(A1,1,function(x) sum(abs(x)))!=0]=1;
       TrueVariables2=rep(0,Q);
       TrueVariables2[apply(A2,1,function(x) sum(abs(x)))!=0]=1;
@@ -259,10 +266,10 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
     
   } else if (scenario==3){
     
-    A1=rbind(V11 ,matrix(0,P-p,4));
+    A1=rbind(V11 ,matrix(0,n_total_features-n_important_features,4));
     A2=rbind(V22 ,matrix(0,Q-q,4)); 
     if (overlap=="no"){
-      whic=matrix(0,P,4);
+      whic=matrix(0,n_total_features,4);
       whic[1:25,1]=1;
       whic[26:50,2]=1;
       whic[51:100,3]=1;
@@ -280,7 +287,7 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       A1[abs(A1)<=10^-8]=0;
       A2[abs(A2)<=10^-8]=0;
     } else if (overlap=="yes"){
-      whic=matrix(0,P,4);
+      whic=matrix(0,n_total_features,4);
       whic[1:50,1]=1;
       whic[1:50,2]=1;
       whic[51:100,3]=1;
@@ -298,7 +305,7 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
       
     }
     
-    TrueVariables1=rep(0,P);
+    TrueVariables1=rep(0,n_total_features);
     TrueVariables1[apply(A1,1,function(x) sum(abs(x)))!=0]=1;
     TrueVariables2=rep(0,Q);
     TrueVariables2[apply(A2,1,function(x) sum(abs(x)))!=0]=1;
@@ -310,11 +317,9 @@ Simulate<-function(scenario=1,setting=NULL,overlap=NULL,seed=1){
   sigma2u=1;
   U=mvrnorm(n,mu=rep(0,r),sigma2u*diag(r))
   A3=c(1, 0, 1, 1)#'; %components 1, 3, and 4 affect response
-  E1=MASS::mvrnorm(n, mu=rep(0,P), Sigma1);
+  E1=MASS::mvrnorm(n, mu=rep(0,n_total_features), Sigma1);
   E2=MASS::mvrnorm(n, mu=rep(0,Q), Sigma2);
   E3=MASS::mvrnorm(n, mu=1, sigma2u);
   X1=U%*%t(A1)+E1;X2=U%*%t(A2)+E2;Y=U%*%A3+E3
   list(X1=X1,X2=X2,Y=Y,TrueVar1=TrueVariables1,TrueVar2=TrueVariables2,U=U,LoadA1=A1,LoadA2=A2,Group1=Group1,Group2=Group2)
 }
-
-
