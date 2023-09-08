53572a7610d130f54c2456cff61ab86cb450304a
Aidan Neher <atm.neher@gmail.com>
2023-09-08 14:19:02 -0500

    update sample_gamma_eta_2.R, simulate_simple_data.R, init woodbury_inverse_matrix.R
    sample_gamma_eta_2.R - updated to reflect understanding in Sigma_j
    
    simulate_simple_data.R - simulate_iid_data() added.
    
    woodbury_inverse_matrix.R - initiated from ChatGPT output.

--- code/sample_gamma_eta_2.R
+++ code/sample_gamma_eta_2.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 
 r <- 4
-n <- 200
-p_m <- 500
+n_obs <- 200
+p_m <- 10
 prob_component_selection <- 0.5
-prob_feature_selection <- 0.05
+prob_feature_selection <- 0.5
 
 initialize_gamma <- function(r=4, prior_component_selection=0.5) {
   gamma <- rbinom(n = r, size = 1, prob = prior_component_selection)
   return(gamma)
 }
-initialize_eta <- function(gamma, r=4, p_m=500, prior_feature_selection=0.05) {
+initialize_eta <- function(gamma, r=4, p_m=10, prior_feature_selection=0.5) {
   eta <- matrix(nrow = r, ncol = p_m)
   for (l in 1:r) {
     if (gamma[l] == 1) {
@@ -28,36 +28,37 @@ eta <- initialize_eta(gamma)
 
 sigma2 <- rep(1, p_m)
 tau2 <- matrix(1, nrow = r, ncol = p_m)
-simulation_results <- readRDS("data/2023-07-03_simulation_results.rds")
+simulation_results <- readRDS("data/2023-09-08_simulation_results.rds")
 U <- simulation_results$U # U is a fixed set of covariates
 
 m <- 1 # Start with the 1st view
-data_list <- readRDS("data/2023-07-03_simulation_data_list.rds")
+data_list <- readRDS("data/2023-09-08_simulation_data_list.rds")
 x <- data_list[[m]]
 
 ## -- calculate P_lj
-calculate_mvnorm_var_j <- function(eta_j, sigma2_j, tau2_j, U, n) {
+calculate_mvnorm_var_j <- function(eta_j, sigma2_j, tau2_j, U, n_obs) {
   n_components_active_in <- sum(eta_j)
   if (n_components_active_in > 0) {
     components_active_in <- which(eta_j==1)
     Sigma2_j <- U[, components_active_in, drop = FALSE] %*% 
-      diag(tau2_j[components_active_in]) %*% 
-      t(U[, components_active_in, drop = FALSE]) + diag(n) # TODO use Woodbury matrix formula for inversion
+      diag(n_components_active_in) %*% 
+      t(U[, components_active_in, drop = FALSE]) + # TODO use Woodbury matrix formula for inversion
+      diag(n_obs) 
   } else {
-    Sigma2_j <- diag(n)
+    Sigma2_j <- diag(n_obs)
   }
   mvnorm_var <- sigma2_j * Sigma2_j
   return(mvnorm_var)
 }
 
-calculate_log_dmvnorm_j <- function(eta_j, sigma2_j, tau2_j, U, n, x_j) {
-  mvnorm_var_j <- calculate_mvnorm_var_j(eta_j, sigma2_j, tau2_j, U, n)
-  log_dmvnorm_j <- mvtnorm::dmvnorm(x = x_j, mean = rep(0, n), sigma = mvnorm_var_j, log = TRUE)
+calculate_log_dmvnorm_j <- function(eta_j, sigma2_j, tau2_j, U, n_obs, x_j) {
+  mvnorm_var_j <- calculate_mvnorm_var_j(eta_j, sigma2_j, tau2_j, U, n_obs)
+  log_dmvnorm_j <- mvtnorm::dmvnorm(x = x_j, mean = rep(0, n_obs), sigma = mvnorm_var_j, log = TRUE)
   return(log_dmvnorm_j)
 }
 
-calculate_log_G_j <- function(gamma, eta_j, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection) {
-  log_dmvnorm_j <- calculate_log_dmvnorm_j(eta_j, sigma2_j, tau2_j, U, n, x_j)
+calculate_log_G_j <- function(gamma, eta_j, sigma2_j, tau2_j, U, n_obs, x_j, prob_feature_selection) {
+  log_dmvnorm_j <- calculate_log_dmvnorm_j(eta_j, sigma2_j, tau2_j, U, n_obs, x_j)
   active_gamma <- which(gamma == 1)
   n_active_gamma <- length(active_gamma) # TODO account for all inactive
   n_active_eta_given_gamma_1 <- eta_j[active_gamma] %>% sum()
@@ -67,12 +68,12 @@ calculate_log_G_j <- function(gamma, eta_j, sigma2_j, tau2_j, U, n, x_j, prob_fe
   return(log_G_j) 
 }
 
-calculate_P_lj <- function(l, gamma, eta_j, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection) {
+calculate_P_lj <- function(l, gamma, eta_j, sigma2_j, tau2_j, U, n_obs, x_j, prob_feature_selection) {
   gamma_1 <- replace(gamma, l, 1)
   eta_1 <- replace(eta_j, l, 1)
   eta_0 <- replace(eta_j, l, 0)
-  log_G_j_1 <- calculate_log_G_j(gamma_1, eta_1, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection)
-  log_G_j_0 <- calculate_log_G_j(gamma_1, eta_0, sigma2_j, tau2_j, U, n, x_j, prob_feature_selection)
+  log_G_j_1 <- calculate_log_G_j(gamma_1, eta_1, sigma2_j, tau2_j, U, n_obs, x_j, prob_feature_selection)
+  log_G_j_0 <- calculate_log_G_j(gamma_1, eta_0, sigma2_j, tau2_j, U, n_obs, x_j, prob_feature_selection)
   max_arg <- max(log_G_j_1, log_G_j_0) # Use logsumexp trick
   a <- log_G_j_1 - max_arg
   b <- log_G_j_0 - max_arg
@@ -81,11 +82,11 @@ calculate_P_lj <- function(l, gamma, eta_j, sigma2_j, tau2_j, U, n, x_j, prob_fe
 }
 
 ## -- Calculate log_target_density_l 
-log_target_density_l <- function(l, gamma, eta, sigma2, tau2, U, n, p_m, 
+log_target_density_l <- function(l, gamma, eta, sigma2, tau2, U, n_obs, p_m, 
                                  x, prob_component_selection, prob_feature_selection) {
   sum_log_target_density_lj <- 0
   for (j in 1:p_m) {
-    log_dmvnorm_j <- calculate_log_dmvnorm_j(eta[, j], sigma2[j], tau2[,j], U, n, x[,j])
+    log_dmvnorm_j <- calculate_log_dmvnorm_j(eta[, j], sigma2[j], tau2[,j], U, n_obs, x[,j])
     # TODO understand if the prior probablity or P_lj should be used in place of prob_feature_selection
     sum_log_target_density_lj <- sum_log_target_density_lj + log_dmvnorm_j + 
       eta[l,j]*log(prob_feature_selection) + (1-eta[l,j])*log(1-prob_feature_selection)
@@ -97,26 +98,36 @@ log_target_density_l <- function(l, gamma, eta, sigma2, tau2, U, n, p_m,
 
 
 ## Testing
-# log_target_0 <- log_target_density_l(l, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
+# log_target_0 <- log_target_density_l(l, gamma, eta, sigma2, tau2, U, n_obs, p_m, x, prob_component_selection, prob_feature_selection)
 # 
 # gamma_1 <- replace(gamma, l, 1)
 # eta_1 <- eta
 # eta_1[l, ] <- rbinom(n = p_m, size = 1, prob = prob_feature_selection)
 # 
-# log_target_1 <- log_target_density_l(l, gamma_1, eta_1, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
+# log_target_1 <- log_target_density_l(l, gamma_1, eta_1, sigma2, tau2, U, n_obs, p_m, x, prob_component_selection, prob_feature_selection)
 # 
 # log_target_0 - log_target_1 # Note, the absolute difference is MUCH smaller in this implementation :-) 
 
+# TODO fix bug in log_proposal_backward
+# log_proposal_backward <- log_proposal_l_density(l, gamma, eta, gamma_new, eta_new, sigma2, tau2, U, n_obs, p_m, x, prob_feature_selection)
+
 ## -- Calculate log_proposal_l
 log_proposal_l_density <- function(l, gamma_prime, eta_prime, gamma, eta,
-                                   sigma2, tau2, U, n, p_m, x, prob_feature_selection) {
+                                   sigma2, tau2, U, n_obs, p_m, x, prob_feature_selection) {
   if (gamma[l]==1 & gamma_prime[l]==0 & sum(eta_prime[l,])==0) {
     return(0)
   } else if (gamma[l]==0 & gamma_prime[l]==1) {
     log_proposal_l_density <- 0
     for (j in 1:p_m) {
-      P_lj <- calculate_P_lj(l, gamma_prime, eta_prime[, j], sigma2[j], tau2[, j], U, n, x[, j], prob_feature_selection)
-      log_proposal_l_density <- log_proposal_l_density + eta_prime[l,j]*log(P_lj) + (1-eta_prime[l,j])*log(1-P_lj)
+      P_lj <- calculate_P_lj(l, gamma_prime, eta_prime[, j], sigma2[j], tau2[, j], U, n_obs, x[, j], prob_feature_selection)
+      # TODO understand if any P_lj's should be 1 or 0 and if so, what a good value for c is
+      c <- 10^-6
+      if (P_lj==1) {
+        P_lj <- P_lj - c
+      } else if (P_lj==0) {
+        P_lj <- P_lj + c
+      }
+      log_proposal_l_density <- log_proposal_l_density + eta_prime[l,j]*log(P_lj) + (1-eta_prime[l,j])*log(1-P_lj) 
     }
     return(log_proposal_l_density)
   } else {
@@ -125,16 +136,16 @@ log_proposal_l_density <- function(l, gamma_prime, eta_prime, gamma, eta,
 }
 
 ## Testing
-# log_proposal_forward <- log_proposal_l_density(l, gamma_1, eta_1, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
-# log_proposal_backward <- log_proposal_l_density(l, gamma, eta, gamma_1, eta_1, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
+# log_proposal_forward <- log_proposal_l_density(l, gamma_1, eta_1, gamma, eta, sigma2, tau2, U, n_obs, p_m, x, prob_feature_selection)
+# log_proposal_backward <- log_proposal_l_density(l, gamma, eta, gamma_1, eta_1, sigma2, tau2, U, n_obs, p_m, x, prob_feature_selection)
 # log_proposal_backward - log_proposal_forward
 # 
 # log_A <- log_target_1 + log_proposal_backward - log_target_0 - log_proposal_forward
 # log_A
 
 ## -- MCMC 2
-n_sample <- 100
-n_burnin <- 0
+n_sample <- 5000
+n_burnin <- 1000
 n_iterations <- n_sample + n_burnin
 
 set.seed(1)
@@ -142,9 +153,9 @@ gamma_chain <- matrix(nrow = r, ncol = n_iterations)
 eta_chain <- array(dim = c(r, p_m, n_iterations))
 gamma <- initialize_gamma()
 eta <- initialize_eta(gamma)
-# iter <- 1
-# l <- 1
+
 for (iter in 1:n_iterations) {
+  print(paste("iter:", iter))
   for (l in 1:r) {
     # Propose values
     gamma_new <- replace(gamma, l, 1-gamma[l])
@@ -155,10 +166,10 @@ for (iter in 1:n_iterations) {
       eta_new[l, ] <- rbinom(n = p_m, size = 1, prob = prob_feature_selection) # Understand if P_lj's should be used
     }
     # Calculate log acceptance ratio
-    log_target <- log_target_density_l(l, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
-    log_target_new <- log_target_density_l(l, gamma_new, eta_new, sigma2, tau2, U, n, p_m, x, prob_component_selection, prob_feature_selection)
-    log_proposal_forward <- log_proposal_l_density(l, gamma_new, eta_new, gamma, eta, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
-    log_proposal_backward <- log_proposal_l_density(l, gamma, eta, gamma_new, eta_new, sigma2, tau2, U, n, p_m, x, prob_feature_selection)
+    log_target <- log_target_density_l(l, gamma, eta, sigma2, tau2, U, n_obs, p_m, x, prob_component_selection, prob_feature_selection)
+    log_target_new <- log_target_density_l(l, gamma_new, eta_new, sigma2, tau2, U, n_obs, p_m, x, prob_component_selection, prob_feature_selection)
+    log_proposal_forward <- log_proposal_l_density(l, gamma_new, eta_new, gamma, eta, sigma2, tau2, U, n_obs, p_m, x, prob_feature_selection)
+    log_proposal_backward <- log_proposal_l_density(l, gamma, eta, gamma_new, eta_new, sigma2, tau2, U, n_obs, p_m, x, prob_feature_selection)
     log_acceptance_ratio <- log_target_new + log_proposal_backward - log_target - log_proposal_forward
     if (log(runif(1)) < log_acceptance_ratio) {
       # Accept proposed gamma and eta
@@ -171,9 +182,13 @@ for (iter in 1:n_iterations) {
     }
     if (gamma[l]==1) {
       # Gibbs sample to mix feature activation indicators
+      # TODO remove for loops
+      P_l <- rep(NA, p_m) 
       for (j in 1:p_m) {
-        P_lj <- calculate_P_lj(l, gamma, eta[, j], sigma2[j], tau2[, j], U, n, x[, j], prob_feature_selection)
-        if (runif(1) < P_lj) {
+        P_l[j] <- calculate_P_lj(l, gamma, eta[, j], sigma2[j], tau2[, j], U, n_obs, x[, j], prob_feature_selection)
+      }
+      for (j in 1:p_m) {
+        if (runif(1) < P_l[j]) {
           # Turn on feature
           eta[l,j] <- 1
         } else {
--- code/simulate_simple_data.R
+++ code/simulate_simple_data.R
@@ -1,4 +1,63 @@
-# Aidan's version:
+n_views=2; n_obs=200; p_m=10 
+prob_feature_importance=0.5; r=4
+prob_component_importance=0.5
+seed=1
+library(tidyverse)
+
+simulate_iid_data <- function(n_views=2, n_obs=200, p_m=10, r=4,
+                              prob_feature_importance=0.5, 
+                              prob_component_importance=0.5,
+                              seed=1) {
+  
+  set.seed(seed)
+  n_important_features <- floor(prob_feature_importance*p_m)
+  n_important_components <- floor(prob_component_importance*r)
+
+  simulate_A <- function(n_important_components, n_important_features) {
+    index_important_components <- seq(to = n_important_components)
+    index_important_features <- seq(to = n_important_features)
+    # simulate_A assumes all features important across active components
+    n_nonzero_a <- n_important_components * n_important_features 
+    nonzero_a <- matrix(rnorm(n_nonzero_a), 
+                        nrow = n_important_components, 
+                        ncol = n_important_features)
+    A <- matrix(0, nrow = r, ncol = p_m) 
+    A[index_important_components, index_important_features] <- nonzero_a
+    return(A)
+  }
+  
+  A_list <- list()
+  for (m in 1:n_views) {
+    A_list[[m]] <- simulate_A(n_important_components, n_important_features)
+  }
+  
+  U <- matrix(data = rnorm(n_obs*r), nrow = n_obs, ncol = r)
+  
+  E_list <- list()
+  for (m in 1:n_views) {
+    E_list[[m]] <- matrix(data = rnorm(n_obs*p_m), nrow = n_obs, ncol = p_m)
+  }
+  
+  X_list <- list()
+  for (m in 1:n_views) {
+    X_list[[m]] <- U %*% A_list[[m]] + E_list[[m]]
+  }
+
+  a <- ifelse(1:r %in% index_important_components, 1, 0) %>%
+    matrix(nrow = r)
+  e <- matrix(rnorm(n_obs), nrow = n_obs)
+    
+  Y <- U %*% a + e
+  
+  simulation_results <- list(X_list=X_list, Y=Y,
+                             index_important_components=seq(n_important_components),
+                             index_important_features=seq(n_important_features),
+                             U=U, A_list=A_list)
+  
+  return(simulation_results)
+}
+
+
 # Note, at present, this version only is functional for scenario 1, setting 1 at p != 500
 simulate_data <- function(n=200, p=10, prob_feature_importance=0.4, r=4, scenario=1, setting=NULL, overlap=NULL, seed=1) {
   
@@ -317,11 +376,11 @@ simulate_data <- function(n=200, p=10, prob_feature_importance=0.4, r=4, scenari
   list(X1=X1,X2=X2,Y=Y,TrueVar1=TrueVariables1,TrueVar2=TrueVariables2,U=U,LoadA1=A1,LoadA2=A2,Group1=Group1,Group2=Group2)
 }
 
-# Start with scenario 1, setting 1
-simulation_results <- simulate_data(setting=1)
+# Start with iid data
+simulation_results <- simulate_iid_data()
 
-data_list <- list(simulation_results$X1, 
-                  simulation_results$X2, 
+data_list <- list(simulation_results$X_list[[1]], 
+                  simulation_results$X_list[[2]], 
                   simulation_results$Y)
 indic_var <- c(0, 0, 1) 
 method <- "BIP" # method without grouping information to start
