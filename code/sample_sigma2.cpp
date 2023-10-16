d95fa8c55dfa667b4f6fc9731fa97bc108c7188c
Aidan Neher <atm.neher@gmail.com>
2023-10-16 11:31:06 -0500

    Update sample_sigma2.cpp
    Include utils.cpp and also draft sample_sigma2 function, which samples sigma for all j in 1:p_m. Testing still required.

--- code/sample_sigma2.cpp
+++ code/sample_sigma2.cpp
@@ -8,8 +8,11 @@
 using namespace arma;
 using namespace Rcpp;
 
+#include "utils.cpp" 
+
 // [[Rcpp::export]]
-arma::vec sample_gamma(int n, float alpha, float beta) {
+float sample_gamma(float alpha, float beta) {
+  
   if (alpha <= 0 || beta <= 0) {
     throw std::runtime_error("Error: Alpha and Beta must be positive.");
   }
@@ -18,31 +21,101 @@ arma::vec sample_gamma(int n, float alpha, float beta) {
   std::random_device rd;
   std::mt19937 gen(rd());
   
-  // Create an inverse gamma distribution
+  // Sample from the gamma distribution
   std::gamma_distribution<float> rgamma_distribution(alpha, beta);
   
-  arma::vec sample(n);
-  for (int i = 0; i < n; i++) {
-    // Generate a random sample from the distribution
-    sample[i] = rgamma_distribution(gen);
-  }
+  float sample = rgamma_distribution(gen);
   
   return sample;
 }
 
-// [[Rcpp::export]]
-float sample_sigma2_j(float alpha_0, float beta_0, int n_obs, vec x_j, mat Sigma_j) {
-  // Check if the length of x_j is equal to n_obs
-  if (x_j.n_elem != n_obs) {
-    // Throw an exception and stop the function
-    throw std::runtime_error("Length of x_j is not equal to n_obs.");
+arma::mat get_Sigma_j(mat U, vec tau_j, uvec eta_j, int n_obs) {
+  
+  mat Sigma_j;
+  
+  int n_components_active_in = arma::sum(eta_j);
+  
+  arma::uvec components_active_in;
+  arma::mat Sigma2_j;
+  
+  if (n_components_active_in > 0) {
+    
+    components_active_in = find(eta_j == 1);
+    mat U_active = U.cols(components_active_in);
+    vec tau_active = tau_j(components_active_in);
+    mat tau_diag = diagmat(tau_active);
+    Sigma2_j = U_active * tau_diag * U_active.t() + eye(n_obs, n_obs);
+    
+  } else {
+    
+    Sigma2_j = eye(n_obs, n_obs);
+    
   }
   
+  return Sigma_j;
   
+}
+
+arma::mat get_Sigma_j_inv(mat U, vec tau_j, uvec eta_j, int n_obs) {
+  
+  mat Sigma_j_inv;
+
+  int n_components_active_in = sum(eta_j);
+  
+  if (n_components_active_in > 0) {
+    
+    uvec components_active_in = find(eta_j == 1);
+    mat U_active = U.cols(components_active_in);
+    vec tau_active = tau_j(components_active_in);
+    mat tau_diag = diagmat(tau_active);
+    Sigma_j_inv = get_woodbury_inv(U_active, tau_diag);
+    
+  } else {
+    
+    Sigma_j_inv = eye(n_obs, n_obs);
+    
+  }
+  
+  return Sigma_j_inv;
+  
+}
+
+// [[Rcpp::export]]
+float sample_sigma2_j(float alpha_0, float beta_0, vec x_j, mat Sigma_j_inv, int n_obs) {
+
   float n_float = static_cast<float>(n_obs); // Convert int to float for division
   float alpha = alpha_0 + n_obs/2;
-  double beta = 1/2 * x_j.t() * Sigma_j.i() * x_j + beta_0;
+  vec beta_vec = 1/2 * x_j.t() * Sigma_j_inv * x_j + beta_0; // This returns a vec of len 1
+  float beta = static_cast<float>(beta_vec(0)); // convert to float for sample_gamma
   
+  float sigma2_j = 1.0/ sample_gamma(alpha, beta);
+  return sigma2_j;
+}
+
+// [[Rcpp::export]]
+vec sample_sigma2(float alpha_0, float beta_0, mat X, 
+                  mat U, mat Tau, mat Eta, int n_obs, int p_m) {
+  
+  vec sigma2;
+  
+  for (int j=0; j < n_obs; j++) {
+    
+    // get_Sigma_j_inv
+    vec tau_j = Tau.col(j);
+    vec eta_j_vec = Eta.col(j);
+    uvec eta_j = conv_to<uvec>::from(eta_j_vec);
+    mat Sigma_j_inv = get_Sigma_j_inv(U, tau_j, eta_j, n_obs);
+    
+    // sample sigma2_j
+    vec x_j = X.col(j);
+    float sigma2_j = sample_sigma2_j(alpha_0, beta_0, x_j, Sigma_j_inv, n_obs);
+    
+    // store sigma2_j
+    sigma2[j] = sigma2_j;
+    
+  }
+  
+  return sigma2;
 }
 
 // You can include R code blocks in C++ files processed with sourceCpp
---
+++ code/utils.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <random>
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+using namespace arma;
+using namespace Rcpp;
+
+// Function to get indices of elements equal to 1
+uvec get_indices_of_ones(uvec x) {
+  int n = x.size();
+  uvec indices;
+  for (int i = 0; i < n; i++) {
+    if (x[i] == 1) {
+      // Create a new uvector one element longer and copy the elements
+      arma::uvec newIndices(indices.n_elem + 1);
+      newIndices.head(indices.n_elem) = indices;
+      // Append the new element to the end
+      newIndices(indices.n_elem) = i;
+      // Replace the original indices with the newIndices if needed
+      indices = newIndices;
+    }
+  }
+  return indices;
+}
+
+// [[Rcpp::export]]
+mat get_woodbury_inv(mat U, mat D) {
+  mat I = eye(U.n_rows, U.n_rows);
+  mat sigma_inv = I - U*inv(inv(D) + U.t() * U) * U.t();
+  return sigma_inv;
+}
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+
+*/
