b7ea0fbbef5e2ff503f5ad41248d8ddd726ba070
Aidan Neher <atm.neher@gmail.com>
2023-10-18 13:24:36 -0500

    Update utils.cpp

--- code/utils.cpp
+++ code/utils.cpp
@@ -33,6 +33,59 @@ mat get_woodbury_inv(mat U, mat D) {
   return sigma_inv;
 }
 
+// [[Rcpp::export]]
+arma::mat get_Sigma_j(mat U, vec tau_j, uvec eta_j, int n_obs) {
+  
+  mat Sigma_j;
+  
+  int n_components_active_in = arma::sum(eta_j);
+  
+  arma::uvec components_active_in;
+  arma::mat Sigma2_j;
+  
+  if (n_components_active_in > 0) {
+    
+    components_active_in = find(eta_j == 1);
+    mat U_active = U.cols(components_active_in);
+    vec tau_active = tau_j(components_active_in);
+    mat tau_diag = diagmat(tau_active);
+    Sigma2_j = U_active * tau_diag * U_active.t() + eye(n_obs, n_obs);
+    
+  } else {
+    
+    Sigma2_j = eye(n_obs, n_obs);
+    
+  }
+  
+  return Sigma_j;
+  
+}
+
+// [[Rcpp::export]]
+arma::mat get_Sigma_j_inv(mat U, vec tau_j, uvec eta_j, int n_obs) {
+  
+  mat Sigma_j_inv;
+  
+  int n_components_active_in = sum(eta_j);
+  
+  if (n_components_active_in > 0) {
+    
+    uvec components_active_in = find(eta_j == 1);
+    mat U_active = U.cols(components_active_in);
+    vec tau_active = tau_j(components_active_in);
+    mat tau_diag = diagmat(tau_active);
+    Sigma_j_inv = get_woodbury_inv(U_active, tau_diag);
+    
+  } else {
+    
+    Sigma_j_inv = eye(n_obs, n_obs);
+    
+  }
+  
+  return Sigma_j_inv;
+  
+}
+
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically 
 // run after the compilation.
