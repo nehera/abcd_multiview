55bcd096d8936f1c5047cd2aa30a2ca24e47a95c
Aidan Neher <atm.neher@gmail.com>
2024-01-19 14:34:49 -0600

    Initialize mainfunction in SamplerhoGamma_translation.cpp
    We still need to initialize datastructures properly for mainfunction and write-out results.

--- code/SamplerhoGamma_translation.cpp
+++ code/SamplerhoGamma_translation.cpp
@@ -20,7 +20,6 @@ using namespace Rcpp;
 #include <RcppGSL.h>
 // [[Rcpp::depends(RcppGSL)]]
 
-// [[Rcpp::export]]
 int logmultigaussianT(const gsl_vector * x, const gsl_vector * y,
                       const gsl_matrix * L,
                       double * result,double *quadF,
@@ -61,8 +60,6 @@ int logmultigaussianT(const gsl_vector * x, const gsl_vector * y,
   
 }
 
-
-// [[Rcpp::export]]
 void findc(int n,bool R[n],int a,int * IDX, int *nx)
 {
   int ii_data[n];
@@ -97,7 +94,6 @@ void findc(int n,bool R[n],int a,int * IDX, int *nx)
   
 }
 
-// [[Rcpp::export]]
 void logGausQuadForm(int j,int r, int n,int p,double ** Tau, double ** U,double ** X,double s2,double* quadForm,bool * Gam,double * loggauss){
   int i,s,s1;
   int NZ1[r];
@@ -160,7 +156,6 @@ void logGausQuadForm(int j,int r, int n,int p,double ** Tau, double ** U,double 
   
 }
 
-// [[Rcpp::export]]
 void SamplerhoGamma(gsl_rng * rr,int r, int n,int IndVar,int p, bool * rho,double ** Tau, double ** U,double ** X, double* q1,double q2,double* s2,double* quadForm,bool** Gam,double *loggauss){
   int l,j;
   bool *rhonew=static_cast<bool*>(malloc(r*sizeof(bool)));
@@ -343,7 +338,174 @@ void SamplerhoGamma(gsl_rng * rr,int r, int n,int IndVar,int p, bool * rho,doubl
   free(rhonew); 
 }
 
+// [[Rcpp::export]]
+void mainfunction(int r, int n, int * P, double 
+    
+    int *Method1,int * n1,int *P,int *r1,
+                  int *Np1,double *datasets,int * IndVar,int *K,
+                  int * Paths,int* maxmodel1, int *nbrsample1,
+                  int *burninsample1,double *CompoSelMean,
+                  double *VarSelMean,double * VarSelMeanGlobal,
+                  double * GrpSelMean,double *GrpEffectMean,
+                  double * IntGrpMean,double * EstU, 
+                  double * EstSig2,double *InterceptMean,
+                  double *EstLoadMod,double *EstLoad,
+                  int *nbrmodel1,double *postgam,
+                  double* priorcompsel,double * priorcompselo,
+                  double* priorb0,double* priorb,
+                  double *priorgrpsel,double *probvarsel){
+  
+  // Initialize parameters
+  
+  
+  // Sample
+  for (t=0;t<N;t++){
+    
+    for (m=0;m<Np;m++){
+      
+      for (int j=0;j<P[m];j++){
+        logGausQuadForm(j, r, n, P[m], Tau[m], U,X1[m], s2[m][j],&quadForm[m][j],Gam[m][j],&loggauss[m][j]);
+        if (t==0){
+          loggauss[m][j]=-DBL_MAX;
+        }
+      }
+      
+      if (IndVar[m]!=2){
+        SamplerhoGamma(rr,r, n,IndVar[m],P[m],rhoest[m],Tau[m], U,X1[m],qv[m],q[m],s2[m],quadForm[m],Gam[m],loggauss[m]);
+      }
+      
+    }
+  }
+  
+  // Report
+
+  
+}
+
 
 /*** R
+library(MASS)
+#dyn.load("~/projects/def-chekouo/chekouo/BayesianFA/CallfromR/BayesianFA.so")
+
+BIP <- function(dataList=dataList,IndicVar=IndicVar, groupList=NULL,Method=Method,nbrcomp=4, sample=5000, burnin=1000,nbrmaxmodels=50,
+                priorcompselv=c(1,1),priorcompselo=c(1,1),priorb0=c(2,2),priorb=c(1,1),priorgrpsel=c(1,1),probvarsel=0.05) {
+  
+  if (sample<=burnin){
+    stop("Argument burnin must be smaller than sample: the number of MCMC iterations.")
+  }
+  if (sample<=20){
+    stop("Please specify a larger number of MCMC iterations")
+  }
+  
+  if (is.null(nbrcomp)){
+    D=which(IndicVar==0);
+    mysvd=lapply(D, function(i)  svd(dataList[[i]]))
+    mysumsvd=lapply(1:length(D), function(i) cumsum(mysvd[[i]]$d)/max(cumsum(mysvd[[i]]$d))*100)
+    KMax=max(unlist(lapply(1:length(D), function(i) min(which(mysumsvd[[i]]>=80, arr.ind = TRUE))))) #chooses maximum from D cumulative proportions
+    nbrcomp=min(KMax+1,10);
+  }
+  
+  
+  #Method="GroupInfo"
+  if (Method=="BIP"){
+    meth=0;
+  } else if (Method=="BIPnet") {
+    meth=1;
+  } else {
+    stop("You must provide either BIP or BIPnet")
+  }
+  
+  Np=length(dataList);P=NULL
+  n=nrow(dataList[[1]])
+  P=NULL;
+  MeanData=list();SD=list();
+  for (i in 1:Np){
+    dataList[[i]]=as.matrix(dataList[[i]])
+    P[i]=ncol(dataList[[i]]);
+    if ((IndicVar[i]==0)||(IndicVar[i]==2)){
+      MeanData[[i]]=apply(dataList[[i]],2,mean);
+      SD[[i]]=apply(dataList[[i]],2,sd);
+      dataList[[i]]=scale(as.matrix(dataList[[i]]),T,T)
+    }
+    dataList[[i]]=t(dataList[[i]]);
+  }
+  datasets=unlist(dataList)
+  
+  if (is.null(groupList) && (meth==1)){
+    stop("You must provide a list of grouping information")
+  } else if (is.null(groupList)){
+    for (ll in 1:length(IndicVar)){
+      groupList[[ll]]=matrix(1,P[ll],1)
+    }
+  }
+  
+  
+  ll=1;
+  K=NULL;
+  for (i in 1:Np){
+    if (IndicVar[i]!=0){
+      K[i]=1;
+    } else {
+      K[i]=ncol(groupList[[ll]]);
+      ll=ll+1;
+    }
+  }
+  groupList=lapply(groupList,t);Paths=unlist(groupList);
+  
+  result <- .C("mainfunction",
+               Method1=as.integer(meth),n1=as.integer(n),P=as.integer(P),r1=as.integer(nbrcomp),Np1=as.integer(Np), datasets=as.double(datasets),IndVar=as.integer(IndicVar), K=as.integer(K), Paths=as.integer(Paths),maxmodel1=as.integer(nbrmaxmodels),                nbrsample1=as.integer(sample), burninsample1=as.integer(burnin),
+               CompoSelMean=as.double(rep(0,Np*nbrcomp)),VarSelMean=as.double(rep(0,nbrcomp*sum(P))),
+               VarSelMeanGlobal=as.double(rep(0,sum(P))),GrpSelMean=as.double(rep(0,nbrcomp*sum(K))),
+               GrpEffectMean=as.double(rep(0,nbrcomp*sum(K))),IntGrpMean=as.double(rep(0,nbrcomp*Np)),
+               EstU=as.double(rep(0,n*nbrcomp)),EstSig2=as.double(rep(0,sum(P))),InterceptMean=as.double(rep(0,1)),
+               EstLoadMod=as.double(rep(0,nbrmaxmodels*nbrcomp*sum(P))),EstLoad=as.double(rep(0,nbrcomp*sum(P))),
+               nbrmodel1=as.integer(0),postgam=rep(0,nbrmaxmodels),priorcompsel=priorcompselv,
+               priorcompselo=priorcompselo,priorb0=priorb0,priorb=as.double(priorb),priorgrpsel=priorgrpsel,probvarsel=as.double(probvarsel));
+  
+  
+  
+  reseffect=result$EstLoadMod;
+  nbrmodel=result$nbrmodel1;
+  EstLoadModel=rep( list(list()), nbrmodel ) 
+  for (mo in 1:nbrmodel){
+    for (m in 1:Np){
+      x=sum(P[1:(m-1)]);y=sum(P[1:m]);
+      if (m==1) {x=0}
+      init=1+x*nbrcomp+(mo-1)*nbrcomp*sum(P);
+      final=y*nbrcomp+(mo-1)*nbrcomp*sum(P);
+      EstLoadModel[[mo]][[m]]=matrix(reseffect[init:final],nbrcomp,byrow=T);
+    }
+  }
+  
+  resvarsel1=result$VarSelMeanGlobal;
+  resvarsel2=result$VarSelMean;
+  resvarsel3=result$GrpSelMean;;
+  resvarsel4=result$GrpEffectMean
+  resvarsel5=result$EstLoad;
+  EstimateU=matrix(result$EstU,n,byrow=T);
+  CompoSelMean=matrix(result$CompoSelMean,Np,byrow=T);
+  IntGrpMean=matrix(result$IntGrpMean,Np,byrow=T);
+  VarSelMeanGlobal=list();
+  VarSelMean=list();
+  GrpSelMean=list();
+  GrpEffectMean=list();
+  EstLoad=list();
+  EstSig2=list();
+  m1=m2=m3=1;
+  for (m in 1:Np){
+    VarSelMeanGlobal[[m]]=resvarsel1[m1:(m1-1+P[m])]
+    VarSelMean[[m]]=matrix(resvarsel2[m2:(m2-1+P[m]*nbrcomp)],P[m],byrow=T)
+    GrpSelMean[[m]]=matrix(resvarsel3[m3:(m3-1+K[m]*nbrcomp)],nbrcomp,byrow=T)
+    GrpEffectMean[[m]]=matrix(resvarsel4[m3:(m3-1+K[m]*nbrcomp)],nbrcomp,byrow=T);
+    EstLoad[[m]]=matrix(resvarsel5[m2:(m2-1+P[m]*nbrcomp)],nbrcomp,byrow=T);
+    EstSig2[[m]]=result$EstSig2[m1:(m1-1+P[m])]
+    m1=m1+P[m];
+    m2=m2+P[m]*nbrcomp;
+    m3=m3+K[m]*nbrcomp;
+    
+  }
+  
+  return (list(EstU=EstimateU,VarSelMean=VarSelMean,VarSelMeanGlobal=VarSelMeanGlobal,CompoSelMean=CompoSelMean,GrpSelMean=GrpSelMean,GrpEffectMean=GrpEffectMean,IntGrpMean=IntGrpMean,EstLoad=EstLoad,EstLoadModel=EstLoadModel,nbrmodel=result$nbrmodel1,EstSig2=EstSig2,EstIntcp=result$InterceptMean,PostGam=result$postgam,IndicVar=IndicVar,nbrcomp=nbrcomp,MeanData=MeanData,SDData=SD))
+}
 
 */
