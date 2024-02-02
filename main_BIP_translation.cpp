7b2f2664168638975dc5452ce7e9672424aee225
Aidan Neher <atm.neher@gmail.com>
2024-02-02 17:20:48 -0600

    Init main_BIP_translation .R and .cpp scripts

---
+++ main_BIP_translation.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <gsl/gsl_rng.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_sf_psi.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_statistics.h>
+#include <gsl/gsl_math.h>
+
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace arma;
+using namespace Rcpp;
+
+#include <RcppGSL.h>
+// [[Rcpp::depends(RcppGSL)]]
+
+////// Utilities //////
+
+////// Sampling subroutines //////
+
+////// Main BIP function //////
+
+
+
+/*** R
+
+*/
